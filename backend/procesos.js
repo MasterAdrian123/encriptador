2a097c4f87229b39e53e6d3532c349018ec305c1
creador <isidrotorresadriandavid@gmail.com>
2024-07-21 22:40:28 -0500

    se añadio un toggle light/dark y se habilito el boton copiar,se cambiaron algunos diseños

--- backend/procesos.js
+++ backend/procesos.js
@@ -4,6 +4,11 @@ const articulo = document.querySelector(".vision");
 const output = document.querySelector(".output");
 const botonCopiar = document.getElementById("copiar");
 const seccionResultado = document.querySelector(".result");
+const toggle = document.getElementById('toggle');
+const contenedorToggle = document.querySelector(".contenedor-toggle");
+const theme = document.querySelector(".theme");
+const input = document.getElementById("input");
+let isDark = false;
 
 // todos los caracteres especiales que no quiero que se inserten en el text area
 const textArea = document.getElementById("input");
@@ -17,26 +22,35 @@ function alertarCaracteresEspeciales(ev){
 }
 textArea.addEventListener('input', alertarCaracteresEspeciales);
 
-
 const visibilizarRespuesta = ()=>{
-    articulo.setAttribute("style","display:none");
-    output.setAttribute("style","display:block");
-    botonCopiar.setAttribute("style","display:block");
+    articulo.style.display="none"
+    output.style.display="block";
+    botonCopiar.style.display="block";
     if(screen.width>1000){
-        seccionResultado.setAttribute("style","justify-content :space-between; padding:2%");
+        seccionResultado.setAttribute("style","justify-content:space-between; padding:2%");
     }else{
-        seccionResultado.setAttribute("style","justify-content :space-between");
+        seccionResultado.setAttribute("style","justify-content: space-between");
+    }
+    if(isDark){
+        seccionResultado.style.backgroundColor= "var(--dark-2)";
+        botonCopiar.style.backgroundColor=" var(--dark-3)";
+        botonCopiar.style.color="white";
     }
 }
 
 const visualizarModoInicial = () =>{
-    articulo.style.display ="";
-    seccionResultado.setAttribute("style","justify-content :space-between;padding:5%");
-    output.setAttribute("style","display:none");
-    botonCopiar.setAttribute("style","display:none");
+    seccionResultado.setAttribute("style","padding:5%");
+    articulo.setAttribute("style","display:flex;flex-direction:column; justify-content:center");
+    output.style.display= "none";
+    botonCopiar.style.display="none";
+    if(isDark){
+        seccionResultado.style.backgroundColor= "var(--dark-2)";
+        botonCopiar.style.backgroundColor=" var(--dark-3)";
+        botonCopiar.style.color="white";
+    }
 }
 const encriptar = ()=>{
-    let mensajeEntrante = document.getElementById("input").value.toLowerCase();
+    let mensajeEntrante = input.value.toLowerCase();
     if(mensajeEntrante.length > 0){
         let mensajeResultante ="";
         for(let i of mensajeEntrante){
@@ -53,7 +67,7 @@ const encriptar = ()=>{
             }
             mensajeResultante += i;
         }
-        output.textContent = mensajeResultante;
+        output.value = mensajeResultante;
         visibilizarRespuesta();
     }else{
         visualizarModoInicial();
@@ -61,18 +75,84 @@ const encriptar = ()=>{
 }
 
 const desencriptar = ()=>{
-    let mensajeEntrante = document.getElementById("input").value.toLowerCase();
+    let mensajeEntrante = input.value.toLowerCase();
     if(mensajeEntrante.length >0){
         let respuesta = mensajeEntrante.replaceAll('enter','e');
         respuesta = respuesta.replaceAll("ai","a");
         respuesta = respuesta.replaceAll("ober","o");
         respuesta = respuesta.replaceAll("ufat","u");
         respuesta = respuesta.replaceAll("imes","i");
-        output.textContent = respuesta;
+        output.value = respuesta;
         visibilizarRespuesta();
     }else{
         visualizarModoInicial();
     }
 }
-botonEncriptar.setAttribute("onclick","encriptar()");
-botonDesencriptar.setAttribute("onclick","desencriptar()");
+
+const darkMode = ()=>{
+    const padreWidth = contenedorToggle.clientWidth;
+    const toggleWidth = toggle.clientWidth;
+    const maxTranslate = padreWidth - toggleWidth - 10;
+    toggle.style.translate = `${maxTranslate}px`;
+    contenedorToggle.style.border = "1px solid yellow"
+    isDark = true;
+
+    // asignar colores oscuros
+
+    document.body.style.backgroundColor= "var(--dark-1)";
+    input.style.color = "white";
+    botonEncriptar.setAttribute("style","background-color:var(--dark-blur);color:white");
+    botonDesencriptar.setAttribute("style","background-color:var(--dark-blue-gray);color:white");
+    seccionResultado.style.backgroundColor= "var(--dark-2)";
+    botonCopiar.style.backgroundColor="var(--dark-3)";
+    document.getElementById("subtitulo").style.color="white";
+    output.style.color="white";
+}
+
+const lightMode = ()=>{
+    toggle.style.translate = '0px';
+    contenedorToggle.style.border = "1px solid black"
+    isDark = false;
+
+    // asignar colores claros
+    document.body.style.backgroundColor= "var(--light-gris)";
+    botonEncriptar.setAttribute("style","background-color:var(--light-brown); color:black");
+    botonDesencriptar.setAttribute("style","background-color:var(--light-rosa-opaque); color:black");
+    // botonCopiar.setAttribute("style","background-color:var(--light-gris); color: black");  
+    botonCopiar.style.backgroundColor="var(--light-gris)";
+    botonCopiar.style.color="black"; 
+    seccionResultado.style.backgroundColor= "var(--light-opaque)";
+    document.getElementById("subtitulo").style.color="black";
+    input.style.color="black";
+    output.style.color="var(--gray-model)";
+}
+
+const iconoSol = ()=>{
+    theme.setAttribute("src","./assets/sol.png");
+}
+
+const iconoLuna = ()=>{
+    theme.setAttribute("src","./assets/luna.png");
+}
+
+const themeMode = ()=>{
+    if(isDark){
+        lightMode();
+        // poner icono de luna
+        iconoLuna();
+    }else{
+        darkMode();
+        // poner icono de sol
+        iconoSol();
+    }
+}
+
+const copiar = ()=>{
+    navigator.clipboard.writeText(output.value);
+    alert("texto copiado");
+}
+
+botonEncriptar.addEventListener('click',encriptar);
+botonDesencriptar.addEventListener("click",desencriptar);
+toggle.addEventListener('click',themeMode);
+botonCopiar.addEventListener('click',copiar);
